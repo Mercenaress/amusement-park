b5d64c9537b52e05bc40a726994ea767fe3ae543
Mercenaress <linden9707@gmail.com>
2023-10-24 14:47:32 +0200

    Added popup modal

---
+++ src/components/AttractionInfoModal.jsx
@@ -0,0 +1,16 @@
+import LargeButton from "./LargeButton";
+
+function AttractionInfoModal() {
+    return ( 
+        <article>
+            <section>
+                <h2>Ferris Wheel</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.</p>
+                <LargeButton buttonText={'Get tickets'}/>
+            </section>
+            <img src="https://cdn.discordapp.com/attachments/453688427133796355/1146038363506356275/image.png?ex=654a1a66&is=6537a566&hm=200e1b166c296eefa24e4882d4d59f42a9ce6cfc2e79437c4fccb66a813b1e68&" alt="" />
+        </article>
+     );
+}
+
+export default AttractionInfoModal;
